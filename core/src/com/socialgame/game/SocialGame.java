43102828dff753b869adbf24679cedc861c43fd2
George Grainger <GraingerGY@cardiff.ac.uk>
2021-01-12 14:27:56 +0000

    Switch to game screen architecture
    SocialGame is now an instance of libGDX Game, and uses different screens to achieve functionality.

--- core/src/com/socialgame/game/SocialGame.java
+++ core/src/com/socialgame/game/SocialGame.java
@@ -1,33 +1,52 @@
 package com.socialgame.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-public class SocialGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class SocialGame extends Game {
+	/**
+	 * Main sprite batch instance used for drawing sprites on screen.
+	 */
+	public SpriteBatch batch;
+
+	/**
+	 * Primary font used for text in game
+	 */
+	public BitmapFont font;
+
+	/**
+	 * Primary sprite sheet.
+	 * Current stores all sprites from the game, might be worth splitting into more sheets if more sprites needed.
+	 */
+	public TextureAtlas spriteSheet;
+
+	/**
+	 * Elapsed time since program start.
+	 * Can be used for timing other game components
+	 */
+	public float elapsedTime;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		spriteSheet = new TextureAtlas();
+		elapsedTime = 0;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		spriteSheet.dispose();
+		font.dispose();
 	}
 }
