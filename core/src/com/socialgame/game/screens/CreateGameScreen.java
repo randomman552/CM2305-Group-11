ae3c2872ecba88705396e6ab419a328eb4e6d6a7
Hayesone <joehayesone@gmail.com>
2021-02-16 01:40:11 +0000

    Added some menu functionality. Needs meat and a link to the game.

---
+++ core/src/com/socialgame/game/screens/CreateGameScreen.java
@@ -0,0 +1,68 @@
+package com.socialgame.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.socialgame.game.SocialGame;
+import com.socialgame.game.player.PlayerCustomisation;
+
+public class CreateGameScreen implements Screen {
+
+    protected final SocialGame game;
+    private Stage stage;
+
+
+    public CreateGameScreen(final SocialGame game) {
+        this.game = game;
+        this.stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        Gdx.input.setInputProcessor(stage);
+        Skin mySkin;
+        mySkin = new Skin(Gdx.files.internal("skin/comic-ui.json"));
+
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
--- desktop/src/com/socialgame/game/desktop/DesktopLauncher.java
+++ desktop/src/com/socialgame/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.socialgame.game.SocialGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920;
+		config.height = 1080;
+		config.fullscreen = false;
+		config.forceExit = true;
 		new LwjglApplication(new SocialGame(), config);
 	}
 }
