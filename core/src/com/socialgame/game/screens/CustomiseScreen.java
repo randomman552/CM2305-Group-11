4f0fdd6d5efd0b62a6531941aca109e948bbfba4
Hayesone <joehayesone@gmail.com>
2021-02-18 16:37:16 +0000

    Added comments to the code and TODOs.

--- core/src/com/socialgame/game/screens/CustomiseScreen.java
+++ core/src/com/socialgame/game/screens/CustomiseScreen.java
@@ -3,17 +3,16 @@ package com.socialgame.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.socialgame.game.SocialGame;
-import com.socialgame.game.player.PlayerCustomisation;
 
 public class CustomiseScreen implements Screen {
 
@@ -45,10 +44,7 @@ public class CustomiseScreen implements Screen {
             }
         });
 
-        Table container = new Table();
-        container.defaults().padTop(10F).padBottom(10F);
-        container.setFillParent(true);
-
+        //TEMP: Labels for the colours
         Label colour1 = new Label("colour",mySkin, "default");
         Label colour2 = new Label("colour",mySkin, "default");
         Label colour3 = new Label("colour",mySkin, "default");
@@ -62,13 +58,52 @@ public class CustomiseScreen implements Screen {
         Label colour11 = new Label("colour",mySkin, "default");
         Label colour12 = new Label("colour",mySkin, "default");
 
+
+        //TEMP: Labels for the playerInfo
         Label playerName = new Label("NAMEEEEEEEEEEE",mySkin, "big");
         Label playerLvl = new Label("Lv.8",mySkin,"big");
         Label playerLvlCurrentBar = new Label("8",mySkin,"big");
         Label playerLvlBar = new Label("##############----",mySkin,"big");
         Label playerLvlNextBar = new Label("9",mySkin,"big");
-        Label label3 = new Label("playerItem Table",mySkin,"default");
 
+        //Buttons for the itemMenu table, currently they do not do anything.
+        Button hatButton = new TextButton("HAT",mySkin,"default");
+        Button topButton = new TextButton("TOP",mySkin,"default");
+
+        Texture itemImageTexture = new Texture("exampleItem.png");
+        Drawable drawable = new TextureRegionDrawable(new TextureRegion(itemImageTexture));
+        ImageButton itemButton = new ImageButton(drawable);
+
+
+        //Save button, to be used to confirm a users choice of customisation
+        Button saveButton = new TextButton("Save",mySkin,"default");
+        saveButton.addListener(new InputListener(){
+
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) { /* touchDown = hovering over button */
+
+                return true;
+            }
+        });
+
+        //Places the user back to the main menu.
+        Button exitButton = new TextButton("Exit",mySkin,"default");
+        exitButton.addListener(new InputListener(){
+
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) { /* touchDown = hovering over button */
+                game.setScreen(new MainMenuScreen(game));
+                return true;
+            }
+        });
+
+
+        //Creates a table that covers the entire screen, and allows nested tables
+        Table container = new Table();
+        container.defaults().padTop(10F).padBottom(10F);
+        container.setFillParent(true);
+
+        // Creates the table for the player to choose colours, TODO: replace labels with buttons
         Table clrPicker = new Table();
             clrPicker.setDebug(true);
             clrPicker.add(colour1).height(Gdx.graphics.getHeight()/14).pad(2f);
@@ -96,6 +131,10 @@ public class CustomiseScreen implements Screen {
             clrPicker.add(colour12).height(Gdx.graphics.getHeight()/14).pad(2f);
             clrPicker.row();
 
+        // Creates the table for the players info
+        // TODO: Replace labels with player data when added to the game.
+        // TODO: Replace level bar with actual bar that represents the progress to next level
+
         Table playerInfoContainer = new Table();
 
             Table playerInfo = new Table();
@@ -107,21 +146,57 @@ public class CustomiseScreen implements Screen {
             playerInfo.add(playerLvlBar);
             playerInfo.add(playerLvlNextBar);
 
+        // TODO: Once items are implemented add preview of picked items for players
             Table playerDisplay = new Table();
             playerDisplay.add(playerDisplayImg);
 
+        // Combines the player display and info a single table
         playerInfoContainer.add(playerInfo).height(Gdx.graphics.getHeight()/14*3);;
         playerInfoContainer.row();
         playerInfoContainer.add(playerDisplay).height(Gdx.graphics.getHeight()/14*11);
 
 
-        Table playerItems = new Table();
-        playerItems.add();
+        // Creates the table to house the item menu side
+        Table playerItemMenuContainer = new Table();
+
+            // Creates the table that houses the items for players to pick.
+            // TODO: Make buttons intractable
+            // TODO: Combine with player preview to see items picked.
+            Table playerItems = new Table();
+            playerItems.defaults().pad(5F);
+            playerItems.add(itemButton);
+            playerItems.add(itemButton);
+            playerItems.add(itemButton);
+            playerItems.row();
+            playerItems.add(itemButton);
+            playerItems.add(itemButton);
+            playerItems.add(itemButton);
+
+            // Table that houses the buttons for nav and items for selection.
+            // TODO: Once items added make it so the two buttons swap items from Hats to Tops
+            Table playerItemMenu = new Table();
+            playerItemMenu.defaults();
+            playerItemMenu.add(hatButton).padRight(Gdx.graphics.getWidth()/12);
+            playerItemMenu.add(topButton);
+            playerItemMenu.row();
+            playerItemMenu.add(playerItems).colspan(2);
+
+            Table thebuttons = new Table();
+            thebuttons.defaults();
+            thebuttons.add(saveButton).padRight(Gdx.graphics.getWidth()/12);
+            thebuttons.add(exitButton);
 
 
+        // Combines the tables with the nav buttons for the game.
+        playerItemMenuContainer.defaults();
+        playerItemMenuContainer.add(playerItemMenu).height(Gdx.graphics.getHeight()/10*8);
+        playerItemMenuContainer.row();
+        playerItemMenuContainer.add(thebuttons).height(Gdx.graphics.getHeight()/10*2);
+
+        //Puts all the tables together in one table.
         container.add(clrPicker).width(Gdx.graphics.getWidth()/18).expandY();
         container.add(playerInfoContainer).width(Gdx.graphics.getWidth()/18*8).expandY();
-        container.add(playerItems).width(Gdx.graphics.getWidth()/18*9).expandY();
+        container.add(playerItemMenuContainer).width(Gdx.graphics.getWidth()/18*9).expandY();
 
         stage.addActor(container);
         stage.setDebugAll(true);
