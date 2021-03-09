f7b18e2857a02b7fe108f02699abfb15f03a6188
George Grainger <GraingerGY@cardiff.ac.uk>
2021-03-09 23:21:01 +0000

    Implement table to store task logic
    BGActor class is now replaced with a table to make UI layout easier.
    Can edit Task.table to create layout.

--- core/src/com/socialgame/game/tasks/Task.java
+++ core/src/com/socialgame/game/tasks/Task.java
@@ -5,12 +5,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
 import com.socialgame.game.SocialGame;
 import com.socialgame.game.baseclasses.GameObject;
@@ -20,38 +21,20 @@ import com.socialgame.game.screens.GameScreen;
 
 /**
  * Class from which all tasks are derived.
- * Functions as a factory, call the createTask method to create a Task
+ * Task can be customised by editing the Task.table attribute after instanciating
  * Extension of Interactable class
  */
 public abstract class Task extends Interactable implements Disposable {
     /**
-     * Actor extension to act as task background (common to all tasks)
+     * Group which is added to the uiStage.
+     * If you want to add elements outside of the primary table, add them here.
      */
-    private static class BGActor extends Actor implements Disposable {
-        private final Texture texture;
-        public BGActor() {
-            Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-            pixmap.setColor(Color.GRAY);
-            pixmap.fill();
-            texture = new Texture(pixmap);
-            pixmap.dispose();
-
-            this.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        }
-
-        @Override
-        public void draw(Batch batch, float parentAlpha) {
-            super.draw(batch, parentAlpha);
-            batch.draw(this.texture, this.getX(), this.getY(), this.getWidth(), this.getHeight());
-        }
-
-        @Override
-        public void dispose() {
-            this.texture.dispose();
-        }
-    }
-
     protected final Group group;
+    /**
+     * Table storing all task logic and elements
+     * Should be edited in sub-classes
+     */
+    protected final Table table;
 
     private boolean complete = false;
     private boolean failed = false;
@@ -63,12 +46,17 @@ public abstract class Task extends Interactable implements Disposable {
         group = new Group();
         close();
 
-        // Add common elements (needed for all tasks)
+        // Create base table
+        Table baseTable = new Table();
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.GRAY);
+        pixmap.fill();
+        baseTable.setBackground(new TextureRegionDrawable(new Texture(pixmap)));
+        pixmap.dispose();
+
+        // Create close button
         ImageButtonStyle style = new ImageButtonStyle();
         ImageButton closeBtn = new ImageButton(style);
-        closeBtn.setBounds(Gdx.graphics.getWidth() - 30, Gdx.graphics.getHeight() - 30, 30, 30);
-
-        // Add InputListener to close task when close button is pressed
         closeBtn.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -76,9 +64,35 @@ public abstract class Task extends Interactable implements Disposable {
                 return true;
             }
         });
+        closeBtn.setSize(50, 50);
 
-        group.addActor(new BGActor());
-        group.addActor(closeBtn);
+        // Define base table size
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+        float marginX = width / 20;
+        float marginY = height / 20;
+        baseTable.setBounds(marginX, marginY, width - (marginX * 2), height - (marginY * 2));
+
+        // Create central table (this is the table the sub-classes will edit)
+        table = new Table();
+
+        // Define base table layout
+        baseTable.row().height(50);
+        baseTable.add().width(50);
+        baseTable.add().expandX();
+        baseTable.add(closeBtn).width(50);
+
+        baseTable.row().expandY();
+        baseTable.add().width(50);
+        baseTable.add(table).fill();
+        baseTable.add().width(50);
+
+        baseTable.row().height(50);
+        baseTable.add().width(50);
+        baseTable.add().expandX();
+        baseTable.add().width(50);
+
+        group.addActor(baseTable);
 
         // Add group to UI Stage, TODO: Add method to game that does this to reduce the cross dependencies
         ((GameScreen) game.getScreen()).uiStage.addActor(group);
