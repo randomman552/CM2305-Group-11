2b417395d3941d5ef2d11dfb7573e39dce564775
JamesMorton1030 <77279971+JamesMorton1030@users.noreply.github.com>
2021-03-09 14:46:10 +0000

    kill button button displayed, not working yet

--- core/src/com/socialgame/game/HUD/HUD.java
+++ core/src/com/socialgame/game/HUD/HUD.java
@@ -5,20 +5,30 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
 import com.socialgame.game.SocialGame;
 import org.graalvm.compiler.phases.common.NodeCounterPhase;
 import org.w3c.dom.Text;
 
+import javax.swing.event.ChangeEvent;
+
 public class HUD extends Group {
     protected SocialGame game;
 
+
     public ProgressBar progressBar;
     public ProgressBar hazardBar;
+    private ImageButton killButton;
 
     public HUD(SocialGame game) {
         this.game = game;
@@ -37,12 +47,16 @@ public class HUD extends Group {
         hazardForegroundPixmap.fill();
 
         // Create texture regions for progress bars to use
-        TextureRegionDrawable background = new TextureRegionDrawable(new TextureRegion(new Texture(progressBackgroundPixmap)));
-        TextureRegionDrawable progressForeground = new TextureRegionDrawable(new TextureRegion(new Texture(progressForegroundPixmap)));
-        TextureRegionDrawable hazardForeground = new TextureRegionDrawable(new TextureRegion(new Texture(hazardForegroundPixmap)));
+        TextureRegionDrawable background = new TextureRegionDrawable(new TextureRegion(
+                new Texture(progressBackgroundPixmap)));
+        TextureRegionDrawable progressForeground = new TextureRegionDrawable(new TextureRegion(
+                new Texture(progressForegroundPixmap)));
+        TextureRegionDrawable hazardForeground = new TextureRegionDrawable(new TextureRegion(
+                new Texture(hazardForegroundPixmap)));
 
         // Set up progress bar
-        com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle progressBarStyle = new com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle();
+        com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle progressBarStyle =
+                new com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle();
         progressBarStyle.background = background;
         progressBarStyle.knob = progressForeground;
         progressBarStyle.knobBefore = progressForeground;
@@ -54,7 +68,8 @@ public class HUD extends Group {
         progressBar.setBounds(10, 10, 100, 30);
 
         // Set up hazard bar
-        com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle hazardBarStyle = new com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle();
+        com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle hazardBarStyle =
+                new com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle();
         hazardBarStyle.knob = hazardForeground;
         hazardBarStyle.knobBefore = hazardForeground;
 
@@ -67,5 +82,27 @@ public class HUD extends Group {
         //Add actors to this HUD group
         addActor(progressBar);
         addActor(hazardBar);
+
+
+        TextureRegionDrawable killButtonDrawable = new TextureRegionDrawable(game.spriteSheet.findRegion("killButton"));
+
+        killButton = new ImageButton(killButtonDrawable);
+
+        killButton.setPosition(2f +100f,
+                2f - 50f);
+        killButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+            }
+        });
+
+        addActor(killButton);
+
+
+
+
+
     }
+
 }
