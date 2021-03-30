5c3890f15bb5696fd6ccfa2fd6d38a550da8a75e
George Grainger <GraingerGY@cardiff.ac.uk>
2021-03-30 01:57:08 +0100

    Functional Simon Says task

--- core/src/com/socialgame/game/tasks/async/SimonSaysTask.java
+++ core/src/com/socialgame/game/tasks/async/SimonSaysTask.java
@@ -3,25 +3,76 @@ package com.socialgame.game.tasks.async;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.socialgame.game.SocialGame;
 import com.socialgame.game.tasks.Task;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
 /**
  * Simon says task based on the task with the same name from the game "Keep Talking and Nobody Explodes"
  * https://ktane.fandom.com/wiki/Simon_Says
+ * TODO Add sound effects
  */
 public class SimonSaysTask extends Task {
+    private static class ButtonInputListener extends InputListener {
+        private final SimonSaysTask task;
+        private final int btnNum;
+
+        public ButtonInputListener(SimonSaysTask task, int btnNum) {
+            this.task = task;
+            this.btnNum = btnNum;
+        }
+
+        @Override
+        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+            return true;
+        }
+
+        @Override
+        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            ((ImageButton) event.getListenerActor()).setChecked(false);
+            task.checkInput(btnNum);
+        }
+    }
+
+    // Sequence storage variables
+    private final ArrayList<Integer> desiredSequence;
+    private final ArrayList<Button> buttons;
+    private int curSequenceIdx = 0;
+
+    // Sequence playing variables
+    private boolean playingSequence = false;
+    private float lastPlayedTime = 0;
+    private final float timePerTile = 1;
+    private int curPlayedIdx = 0;
+
     public SimonSaysTask(SocialGame game, float x, float y) {
         super(game, x, y);
+        lastPlayedTime = game.elapsedTime;
 
-        // Define colours for use with buttons
+        //region Random sequence generation
+        desiredSequence = new ArrayList<>(Arrays.asList(0,1,2,3));
+
+        Random random = new Random();
+        Collections.shuffle(desiredSequence, random);
+        //endregion
+
+        //region Button colours
         Color c1 = new Color(1, 0, 0, 1);
         Color c2 = new Color(0, 1, 0, 1);
         Color c3 = new Color(0, 0, 1, 1);
         Color c4 = new Color(1, 1, 0, 1);
+        //endregion
 
         // Pixmap (reused for setting all button colours)
         Pixmap pixmap = new Pixmap(150, 150, Pixmap.Format.RGBA8888);
@@ -35,6 +86,7 @@ public class SimonSaysTask extends Task {
         TextureRegionDrawable rBtnDown = new TextureRegionDrawable(new Texture(pixmap));
 
         ImageButton rBtn = new ImageButton(rBtnUp, rBtnDown, rBtnDown);
+        rBtn.addListener(new ButtonInputListener(this, 0));
         //endregion
 
         //region Create green button
@@ -46,6 +98,7 @@ public class SimonSaysTask extends Task {
         TextureRegionDrawable gBtnDown = new TextureRegionDrawable(new Texture(pixmap));
 
         ImageButton gBtn = new ImageButton(gBtnUp, gBtnDown, gBtnDown);
+        gBtn.addListener(new ButtonInputListener(this, 1));
         //endregion
 
         //region Create blue button
@@ -57,6 +110,7 @@ public class SimonSaysTask extends Task {
         TextureRegionDrawable bBtnDown = new TextureRegionDrawable(new Texture(pixmap));
 
         ImageButton bBtn = new ImageButton(bBtnUp, bBtnDown, bBtnDown);
+        bBtn.addListener(new ButtonInputListener(this, 2));
         //endregion
 
         //region Create yellow button
@@ -68,6 +122,7 @@ public class SimonSaysTask extends Task {
         TextureRegionDrawable yBtnDown = new TextureRegionDrawable(new Texture(pixmap));
 
         ImageButton yBtn = new ImageButton(yBtnUp, yBtnDown, yBtnDown);
+        yBtn.addListener(new ButtonInputListener(this, 3));
         //endregion
 
         //region Create inner table
@@ -77,6 +132,9 @@ public class SimonSaysTask extends Task {
         innerTable.row();
         innerTable.add(bBtn).size(150);
         innerTable.add(yBtn).size(150);
+
+        // Store buttons in array for later use
+        buttons = new ArrayList<Button>(Arrays.asList(rBtn, gBtn, bBtn, yBtn));
         //endregion
 
         // Add to main table
@@ -85,5 +143,65 @@ public class SimonSaysTask extends Task {
 
         // Dispose of no longer required objects to prevent memory leaks
         pixmap.dispose();
+    }
+
+    /**
+     * Method to be called to begin the sequence playing.
+     */
+    private void playSequence() {
+        playingSequence = true;
+        lastPlayedTime = game.elapsedTime - timePerTile;
+        for (Button button: buttons) {
+            button.setTouchable(Touchable.disabled);
+        }
+    }
+
+    private void handlePlaySequence() {
+        if (lastPlayedTime + timePerTile <= game.elapsedTime) {
+            Button curButton = buttons.get(desiredSequence.get(Math.min(curPlayedIdx, desiredSequence.size() - 1)));
+            Button prevButton = buttons.get(desiredSequence.get(Math.max(curPlayedIdx - 1, 0)));
+
+            prevButton.setChecked(false);
+            curButton.setChecked(true);
+
+            lastPlayedTime = game.elapsedTime;
+            curPlayedIdx++;
+
+            if (curPlayedIdx > desiredSequence.size()) {
+                playingSequence = false;
+                curPlayedIdx = 0;
+                for (Button button: buttons) {
+                    button.setTouchable(Touchable.enabled);
+                    button.setChecked(false);
+                }
+            }
+
+        }
+    }
+
+    private void checkInput(int val) {
+        if (desiredSequence.get(curSequenceIdx) != val) {
+            curSequenceIdx = 0;
+            playSequence();
+            return;
+        }
+
+        // Check if we have reached the end of the desired sequence.
+        curSequenceIdx++;
+        if (curSequenceIdx >= desiredSequence.size()) {
+            complete = true;
+        }
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        if (playingSequence) handlePlaySequence();
+    }
+
+    @Override
+    public void open() {
+        playSequence();
+        super.open();
     }
 }
