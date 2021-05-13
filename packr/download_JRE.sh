2d202a41b669d3d5d5b9481b7e1295f45b0a40a2
George Grainger <GraingerGY@cardiff.ac.uk>
2021-05-13 15:23:37 +0100

    Add additional builds to config

---
+++ packr/download_JRE.sh
@@ -0,0 +1,3 @@
+wget -O Java_Windows.zip https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x86-32_windows_hotspot_11.0.11_9.zip
+wget -O Java_Linux.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_linux_hotspot_11.0.11_9.tar.gz
+wget -O Java_Mac.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_mac_hotspot_11.0.11_9.tar.gz
