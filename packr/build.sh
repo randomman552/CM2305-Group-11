ef70df96d8a36f2eda8315e187bc59f6b7ba8f0e
George Grainger <GraingerGY@cardiff.ac.uk>
2021-05-13 15:34:43 +0100

    Simplify github build

---
+++ packr/build.sh
@@ -0,0 +1,15 @@
+# Get JRE's
+wget -q -O Java_Windows.zip https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x86-32_windows_hotspot_11.0.11_9.zip
+wget -q -O Java_Linux.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_linux_hotspot_11.0.11_9.tar.gz
+wget -q -O Java_Mac.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_mac_hotspot_11.0.11_9.tar.gz
+
+# Run build
+java -jar packr-all.jar windows.json
+java -jar packr-all.jar linux.json
+java -jar packr-all.jar mac.json
+
+# Zip results
+cd out
+run: zip -r windows.zip ./windows
+run: zip -r linux.zip ./linux
+run: zip -r mac.zip ./mac
