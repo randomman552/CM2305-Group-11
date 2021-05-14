bb070b615771f0a073c88bf7f8c93c3987551636
George Grainger <GraingerGY@cardiff.ac.uk>
2021-05-14 17:07:28 +0100

    Fix wrong java version on build

---
+++ README.md
@@ -0,0 +1,7 @@
+# [Ugly Duckling](https://github.com/randomman552/CM2305-Group-11)
+Group project for CM2305.  
+Made by Group 11
+
+## Running
+To run the program, please download the release relevant to your OS from 
+[releases](https://github.com/randomman552/CM2305-Group-11/releases).
--- packr/build.sh
+++ packr/build.sh
@@ -1,5 +1,5 @@
 # Get JRE's
-wget -q -O Java_Windows.zip https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x86-32_windows_hotspot_11.0.11_9.zip
+wget -q -O Java_Windows.zip https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/11.0.8%2B10/openlogic-openjdk-jre-11.0.8%2B10-windows-x64.zip
 wget -q -O Java_Linux.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_linux_hotspot_11.0.11_9.tar.gz
 wget -q -O Java_Mac.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_mac_hotspot_11.0.11_9.tar.gz
 
